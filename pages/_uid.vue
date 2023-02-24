d3a8f0ca5e6baef090e1577954ebc749bcb59e65
Quim Zudaire <quimzudaire@gmail.com>
2023-02-24 08:46:25 +0100

    upd

--- pages/_uid.vue
+++ pages/_uid.vue
@@ -1,26 +1,46 @@
 <template>
-  <SliceZone :slices="page.data.slices" :components="components" />
+    <div>
+      <SliceZone :slices="page.data.slices" :components="components" />
+
+      <section v-if="isProjectes"><
+      <article  v-for="post in posts">
+        <nuxt-link :to='""+post.slugs[0]'>
+          <PrismicImage :field="post.data.imatge_destacada" />
+        </nuxt-link>   
+        </article>
+      </section>
+    </div>
 </template>
 
 <script>
 import { components } from '~/slices'
 
 export default {
-  async asyncData ({ $prismic, params, store, i18n }) {
-    const lang = i18n.locale
-    const page = await $prismic.api.getByUID('page', params.uid, { lang })
-    await store.dispatch('prismic/load', { lang, page })
-    return {
-      page
-    }
-  },
   data () {
     return { components }
   },
+  async asyncData ({ $prismic, params, store, i18n }) {
+    const lang = i18n.locale
+    const page = await $prismic.api.getByUID('page', params.uid, { lang })
+    const { results: posts } = await $prismic.api.query($prismic.predicate.at('document.type', 'projectes'),{lang}
+      )
+      await store.dispatch('prismic/load', { lang, page })
+
+    return {
+      page,
+      posts
+    }
+  },
+
   head () {
     return {
       title: `${this.$prismic.asText(this.page.data.title)} | ${this.$prismic.asText(this.$store.state.prismic.settings.data.siteTitle)}`
     }
+  },
+  computed: {
+    isProjectes() {
+     return this.$route.params.uid === 'projecte'
+  }
   }
 }
 </script>
--- pages/projectes/00index.vue
+++ pages/projectes/00index.vue
@@ -4,15 +4,14 @@
       <h1 v-if="page.lang == 'es-es'">Proyectos</h1>
      
       <section>
-        <template v-for="post in posts">
-          <div class="projecte"><a :href="$route.path + '/' + post.slugs[0]">
-            <figure class="img_projecte">
-              <img :src="post.data.imatge_destacada.url">
-            </figure>
-            <h2>{{ post.data.titol_projecte[0].text }}</h2>
-            <p>{{ post.data.codi_projecte }}</p>
-          </a></div>
-        </template>
+        <article v-for="post in posts">
+          1
+        <!--<PrismicImage :field="post.data.imatge_destacada" />
+        {{ post.results.data.imatge_destacada }} 
+        -->
+        <PrismicImage :field="post.data.imatge_destacada" />
+        {{ post.data }}
+        </article>
       </section>
     </main>
   </template>
@@ -21,18 +20,17 @@
 import { components } from '~/slices'
 
   export default {
-  
-    
+     
     
     async asyncData({ $prismic, store, error , params, i18n}) {
       // Query last posts
       const lang = i18n.locale
       const page = await $prismic.api.getByUID('page', 'projecte', { lang })
-        const posts = await $prismic.api.query(
+      const posts = await $prismic.api.query(
             $prismic.predicate.at('document.type', 'projectes'),
             {lang}
       )
-      await store.dispatch('prismic/load', { lang, page })
+      await store.dispatch('prismic/load', { lang, page, posts })
 
       
 
