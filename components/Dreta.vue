c0a40904fd61b4d0c581cf9b77c675fd4233c8fb
Quim Zudaire <quimzudaire@gmail.com>
2023-03-06 11:15:15 +0100

    UPD

---
+++ components/Dreta.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <nav class="contactes_vertical"><ul class>
+      <li v-for="contacte in navigation.data.contacte_menu">
+        <PrismicLink :field="contacte.link_contacte">
+          <span>{{ contacte.nom_contacte[0].text }}</span>
+        </PrismicLink>
+      </li>
+    </ul></nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object,
+      required: true
+    },
+    settings: {
+      type: Object,
+      required: true
+    },
+    alternateLanguages: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
---
+++ components/Esquerra.vue
@@ -0,0 +1,31 @@
+<template>
+
+
+    <nav class="xarxes_vertical"><ul class>
+      <li v-for="xxss in navigation.data.xxss_menu">
+        <PrismicLink :field="xxss.link_xxss">
+          <span>{{ xxss.nom_xxss[0].text }}</span>
+        </PrismicLink>
+      </li>
+    </ul></nav>
+
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object,
+      required: true
+    },
+    settings: {
+      type: Object,
+      required: true
+    },
+    alternateLanguages: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
