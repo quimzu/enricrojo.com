93dc41d8ab0f53f0fa5a0120b6a0b4131a7155a3
Jaumo <83287129+Jaumo19@users.noreply.github.com>
2023-03-09 17:22:22 +0100

    a
    a

--- pages/projecte/_uid.vue
+++ pages/projecte/_uid.vue
@@ -19,18 +19,6 @@
 
 
         <SliceZone :slices="post.data.slices" :components="components"/>
-
-        <ul>
-		<h2>idioma</h2>
-		<li
-            v-for="lang in alternateLanguages"
-            :key="lang.lang"
-          >
-            <PrismicLink :field="{ ...lang, link_type: 'Document' }">
-              <span class="sr-only">{{ lang.lang }}</span>
-            </PrismicLink>
-          </li>
-	  </ul>
       <!--  <template v-if="post.data.slices[0].items[0] != null">
         <div class='slideshow-container'>
           <template v-for="img in post.data.slices[0].items ">
@@ -38,11 +26,53 @@
                 <img :src="img.imatge.url" style="width:100%">
             </div>
           </template>
-          <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+          <a class="prev" onclick="plusSlides(-1)">&#10094;</nuxt-link>
+        <a class="next" onclick="plusSlides(1)">&#10095;</nuxt-link>
         <br>
         </div>
         </template>-->
+
+
+    <template v-if="post.lang == 'ca'">
+      <section class="projectes_relacionats" v-if="ProjecteRelacionat1 != null || ProjecteRelacionat2 != null || ProjecteRelacionat3 != null">
+        <h3>PROJECTES RELACIONATS</h3>
+        <div>
+          <article v-if="ProjecteRelacionat1 != null">
+            <img :src="ProjecteRelacionat1.data.imatge_destacada_1.url">
+            <nuxt-link :to='"/projecte/"+post.data.projecte_relacionat_1.uid' ><h4>{{ ProjecteRelacionat1.data.titol_projecte[0].text }}</h4></nuxt-link>
+          </article>
+          <article v-if="ProjecteRelacionat2 != null">
+            <img :src="ProjecteRelacionat2.data.imatge_destacada_1.url">
+            <nuxt-link :to='"/projecte/"+post.data.projecte_relacionat_2.uid' ><h4>{{ ProjecteRelacionat2.data.titol_projecte[0].text }}</h4></nuxt-link>
+          </article>
+          <article v-if="ProjecteRelacionat3 != null">
+            <img :src="ProjecteRelacionat3.data.imatge_destacada_1.url">
+            <nuxt-link :to='"/projecte/"+post.data.projecte_relacionat_3.uid' ><h4>{{ ProjecteRelacionat3.data.titol_projecte[0].text }}</h4></nuxt-link>
+          </article>
+        </div>
+      </section>
+    </template>
+    <template v-else>
+      <section class="projectes_relacionats" v-if="ProjecteRelacionat1 != null || ProjecteRelacionat2 != null || ProjecteRelacionat3 != null">
+        <h3>PROYECTOS RELACIONADOS</h3>
+        <div>
+          <article v-if="ProjecteRelacionat1 != null">
+            <img :src="ProjecteRelacionat1.data.imatge_destacada_1.url">
+            <nuxt-link :to='"/es-es/projecte/"+post.data.projecte_relacionat_1.uid' ><h4>{{ ProjecteRelacionat1.data.titol_projecte[0].text }}</h4></nuxt-link>
+          </article>
+          <article v-if="ProjecteRelacionat2 != null">
+            <img :src="ProjecteRelacionat2.data.imatge_destacada_1.url">
+            <nuxt-link :to='"/es-es/projecte/"+post.data.projecte_relacionat_2.uid' ><h4>{{ ProjecteRelacionat2.data.titol_projecte[0].text }}</h4></nuxt-link>
+          </article>
+          <article v-if="ProjecteRelacionat3 != null">
+            <img :src="ProjecteRelacionat3.data.imatge_destacada_1.url">
+            <nuxt-link :to='"/es-es/projecte/"+post.data.projecte_relacionat_3.uid' ><h4>{{ ProjecteRelacionat3.data.titol_projecte[0].text }}</h4></nuxt-link>
+          </article>
+        </div>
+      </section>
+    </template>
+
+
     </main>
 </template>
   
@@ -54,9 +84,15 @@ export default {
     async asyncData ({ $prismic, params, store, i18n }) {
         const lang = i18n.locale
         const post = await $prismic.api.getByUID("projectes", params.uid, { lang });
+        const ProjecteRelacionat1 = await $prismic.api.getByUID("projectes", post.data.projecte_relacionat_1.uid, { lang });
+        const ProjecteRelacionat2 = await $prismic.api.getByUID("projectes", post.data.projecte_relacionat_2.uid, { lang });
+        const ProjecteRelacionat3 = await $prismic.api.getByUID("projectes", post.data.projecte_relacionat_3.uid, { lang });
         await store.dispatch('prismic/load', { lang, page: post })
         return {
             post,
+            ProjecteRelacionat1,
+            ProjecteRelacionat2,
+            ProjecteRelacionat3,
         };
     },
     data () {
